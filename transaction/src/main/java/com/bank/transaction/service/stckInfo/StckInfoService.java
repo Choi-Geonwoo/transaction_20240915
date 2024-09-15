5326b1ef8c48fbbb2a85c4d803a4d76043a160bb
[user name] <invrs@naver.com>
2024-09-15 23:51:58 +0900

    3차 업로드

---
+++ transaction/src/main/java/com/bank/transaction/service/stckInfo/StckInfoService.java
@@ -0,0 +1,33 @@
+package com.bank.transaction.service.stckInfo;
+
+import java.util.List;
+import java.util.Map;
+
+/**
+* @packageName    : com.bank.transaction.service.stckInfo(주식정보)
+* @fileName       : StckInfoService.java
+* @author         : Jihun Park
+* @date           : 2024.09.15
+* @description    :
+* ===========================================================
+* DATE              AUTHOR             NOTE
+* -----------------------------------------------------------
+* 2024.09.15        Jihun Park       최초 생성
+*/
+
+public interface StckInfoService {
+	
+	/**
+		* @packageName    : com.bank.transaction.service.stckInfo(주식정보)
+		* @fileName       : stckInfoSelect.java(주식정보 조회)
+		* @author         : Jihun Park
+		* @date           : 2024.09.15
+		* @description    :
+		* ===========================================================
+		* DATE              AUTHOR             NOTE
+		* -----------------------------------------------------------
+		* 2024.09.15        Jihun Park       최초 생성
+	**/
+	public List<Map<String, Object>> stckInfoSelect(Map<String, Object> mapData);
+
+}
