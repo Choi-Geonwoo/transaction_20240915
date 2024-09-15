7aed17a9c2a0a8c093016cfc7baefc4a1ba6bccb
[user name] <invrs@naver.com>
2024-09-15 21:54:53 +0900

    테스트

--- transaction/src/main/java/com/bank/transaction/TransactionApplication.java
+++ transaction/src/main/java/com/bank/transaction/TransactionApplication.java
@@ -7,7 +7,6 @@ import org.springframework.boot.web.servlet.support.SpringBootServletInitializer
 
 @SpringBootApplication
 public class TransactionApplication {
-	
 	public static void main(String[] args) {
 		SpringApplication.run(TransactionApplication.class, args);
 	}
